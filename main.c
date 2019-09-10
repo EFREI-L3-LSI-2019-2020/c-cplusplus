28293ecee9fe23c4e83efdd0608eef7202bef3e9
Thomas LACAZE <thomaslacaze@MacBook-Pro-de-Thomas.local>
2019-09-10 13:40:41 +0200

    need to fix printf float in D
    Co-authored-by: PoloLacoste <jm@remeur.fr>

--- main.c
+++ main.c
@@ -2,26 +2,163 @@
 
 int main()
 {
-	char *str = NULL;
+	/* 	char *str = NULL;
 	size_t size = 0;
 	printf("Donnez un titre à votre message: ");
-	getline(&str, &size, stdin);
+	getline(&str, &size, stdin); */
+	char str[1000] = "le magasin ouvrira le 12 juin et le 14 juin.";
 
-	size_t ln = strlen(str) - 1;
+	/*  	size_t ln = strlen(str) - 1;
 	if (*str && str[ln] == '\n')
-		str[ln] = '\0';
+		str[ln] = '\0'; */
 
+	remove_special_char_tolower(str);
 	PresentList list = Q_a_b(copystr(str));
 	Q_c(copystr(str), "le", list);
-	//Q_d(list);
+	Q_d(list);
 
-	//char str[1000] = "Une personne m'a dit le mot bonjour. J'ai répondu bonjour à cette personne! Bonjour? Quel joli mot!";
 	return 0;
+}
+
+PresentList Q_a_b(char str[])
+{
+	printf("************************\n");
+	printf("*         Q_a_b        *\n");
+	printf("************************\n");
+
+	Present *freq = (Present *)calloc(1, sizeof(Present));
+	char *pch = strtok(str, " ");
+	int ite = 0;
+	while (pch != NULL)
+	{
+		int present = 1;
+		for (int i = 0; i < ite; i++)
+		{
+			if (strcmp(freq[i].word, pch) == 0)
+			{
+				freq[i].count++;
+				present = 0;
+			}
+		}
+
+		if (present)
+		{
+			freq = (Present *)realloc(freq, (ite + 1) * sizeof(Present));
+			freq[ite] = (Present){pch, 1};
+			ite++;
+		}
+
+		pch = strtok(NULL, " ");
+	}
+	free(pch);
+	free(str);
+
+	affichage(freq, ite);
+
+	return (PresentList){freq, ite};
+}
+
+void Q_c(char str[], char search[], PresentList list)
+{
+	printf("************************\n");
+	printf("*          Q_c         *\n");
+	printf("************************\n");
+
+	Present *dico = NULL;
+	int ite = 0;
+
+	for (int i = 0; i < list.length; i++)
+	{
+		int new = 0;
+		if (list.present[i].count > 1)
+		{
+			for (int j = 0; j < ite; j++)
+			{
+				if (strcmp(dico[j].word, list.present[i].word) == 0)
+				{
+					new = 1;
+				}
+			}
+
+			if (new == 0)
+			{
+				dico = (Present *)realloc(dico, (ite + 1) * sizeof(Present));
+				dico[ite] = list.present[i];
+				ite++;
+			}
+		}
+	}
+
+	char *result = (char *) malloc(sizeof(char));
+	char *pch = strtok(str, " ");
+
+	while (pch != NULL)
+	{
+		int size = strlen(pch);
+		char temp[size];
+		strcpy(temp, pch);
+
+		for (int i = 0; i < ite; i++)
+		{
+			if (strcmp(pch, dico[i].word) == 0)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					temp[j] = '#';
+				}
+			}
+		}
+		strcat(temp, " ");
+		result = (char *) realloc(result, (strlen(result) + strlen(temp)) * sizeof(char));
+		strcat(result, temp);
+
+		pch = strtok(NULL, " ");
+	}
+
+	affichage(dico, ite);
+	printf("%s\n", result);
+
+	free(result);
+	free(dico);
+	free(pch);
+	free(str);
+}
+
+void Q_d(PresentList list)
+{
+	printf("************************\n");
+	printf("*          Q_d         *\n");
+	printf("************************\n");
+
+	int total = 0;
+
+	for (int i = 0; i < list.length; i++)
+	{
+		total += list.present[i].count;
+	}
+
+	int freq[list.length];
+
+	for (int i = 0; i < list.length; i++)
+	{
+		printf("%d / %d\n", list.present[i].count, total);
+		float pour = (int)list.present[i].count / (int)total;
+		printf("%s : %.2f\n", list.present[i].word, pour);
+	}
+
+	int a, y;
+    float b, c, d;
+    a = 750;
+	y = 350;
+    b = a / 350.0f;
+    c = 750;
+    d = c / y;
+    printf("%.2f %.2f\n", b, d);
 }
 
 char *copystr(char str[])
 {
-	char *origin = malloc(strlen(str) * sizeof(char));
+	char *origin = (char *)calloc(strlen(str), sizeof(char));
 	strcpy(origin, str);
 	return origin;
 }
@@ -49,137 +186,6 @@ void affichage(Present *freq, int ite)
 {
 	for (int i = 0; i < ite; i++)
 	{
-		printf("%s:%d\n\n", freq[i].word, freq[i].count);
+		printf("%s:%d\n", freq[i].word, freq[i].count);
 	}
-}
-
-PresentList Q_a_b(char str[])
-{
-	printf("************************\n");
-	printf("*         Q_a_b        *\n");
-	printf("************************\n");
-
-	Present *freq = malloc(sizeof(Present));
-	remove_special_char_tolower(str);
-	char *pch = strtok(str, " ");
-	int ite = 0;
-	while (pch != NULL)
-	{
-		int present = 0;
-		for (int i = 0; i < ite; i++)
-		{
-			if (strcmp(freq[i].word, pch) == 0)
-			{
-				freq[i].count++;
-				present = 1;
-			}
-		}
-
-		if (present == 0)
-		{
-			freq = realloc(freq, (ite + 1) + sizeof(Present));
-			freq[ite] = (Present){pch, 1};
-			ite++;
-		}
-
-		pch = strtok(NULL, " ");
-	}
-	//free(pch);
-	free(str);
-
-	affichage(freq, ite);
-
-	return (PresentList){freq, ite};
-}
-
-void Q_c(char str[], char search[], PresentList list)
-{
-	printf("************************\n");
-	printf("*          Q_c         *\n");
-	printf("************************\n");
-
-	printf("Start : %lu\n", strlen(str));
-
-	Present *dico = malloc(sizeof(Present));
-	remove_special_char_tolower(str);
-	char *pch = strtok(str, " ");
-	int ite = 0;
-
-	for (int i = 0; i < list.length; i++)
-	{
-		int new = 0;
-		if (list.present[i].count > 1)
-		{
-			for (int j = 0; j < ite; j++)
-			{
-				if (strcmp(dico[j].word, list.present[i].word) == 0)
-				{
-					new = 1;
-				}
-			}
-
-			if (new == 0)
-			{
-				dico = realloc(dico, (ite + 1) + sizeof(list.present[i]));
-				dico[ite] = list.present[i];
-				ite++;
-			}
-		}
-	}
-
-	char *result = malloc(sizeof(char));
-
-	while (pch != NULL)
-	{
-		int size = strlen(pch);
-		char temp[size];
-		strcpy(temp, pch);
-
-		for (int i = 0; i < ite; i++)
-		{
-			if (strcmp(pch, dico[i].word) == 0)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					temp[j] = '#';
-				}
-			}
-		}
-		strcat(temp, " ");
-		strcat(result, temp);
-
-		pch = strtok(NULL, " ");
-	}
-	free(pch);
-	free(str);
-
-	//affichage(dico, ite);
-	printf("%s\n", result);
-
-	//printf("End : %lu\n", strlen(result));
-
-	free(result);
-}
-
-void Q_d(PresentList list)
-{
-	printf("************************\n");
-	printf("*          Q_d         *\n");
-	printf("************************\n");
-
-	affichage(list.present, list.length);
-
-	/*int total = 0;
-
-	for(int i = 0; i < list.length; i++)
-	{
-		total += list.present[i].count;
-	}
-
-	int freq[list.length];
-
-	for(int i = 0; i < list.length; i++)
-	{
-		printf("%s : %d : %d\n",list.present[i].word, list.present[i].count, total);
-	}*/
 }
